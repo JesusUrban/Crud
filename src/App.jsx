55eb39e6e9dce74f73bf5709a673db3145c94c13
JesusUrban <142059202+JesusUrban@users.noreply.github.com>
2025-01-28 21:14:30 -0500

    First-commit

---
+++ src/App.jsx
@@ -0,0 +1,183 @@
+import React, { useEffect, useState } from 'react'
+import useCrudApi from './hooks/useCrudApi'
+
+// 
+
+const initialValues = {
+    first_name:'',
+    last_name:'',
+    email: '',
+    password: '',
+    birthday: '',
+    image_url:''
+}
+
+const baseUrl = 'https://users-crud-api-production-9c59.up.railway.app/api/v1/'
+
+function App() {
+ const {request, data: users, pending, error} = useCrudApi()
+ const [values, setValues] = useState(initialValues)
+ const [edit, setEdit] = useState(null)
+
+ useEffect(() => {
+ request({url: baseUrl + 'users'})
+ 
+ }, [])
+
+const add  = (user)=>{
+  
+ request({
+  url: baseUrl +`users/${id}`,
+  method: 'POST',
+  body: user
+ })
+ 
+}
+
+const handleEdit  = ( ) => {
+  console.log('editing')
+}
+const remove = (id) =>{
+  console.log('remove', id)
+  request({
+  url: baseUrl +'users',
+  method: 'DELETE',
+  id
+
+  })
+}
+
+ const handleChange = ({name, value}) =>{
+  
+  
+  setValues({
+    ...values, [name]: value
+  })
+  
+
+}
+
+ const handleSubmit = (e) =>{
+  e.preventDefault()
+  
+  add(values)
+ }
+
+
+ return (
+    <div>
+ <form onSubmit={handleSubmit} className='w-64 px-2 mt-5' action=""> 
+      <div className='mb-4'>
+        <label className='label'>
+        First Name
+         <input 
+         className='input'
+         type="text"
+         name='first_name'
+         value={values.first_name}
+         onChange={(e) => handleChange(e.target)}
+          />
+        </label>
+      </div>
+      <div className='mb-4'>
+        <label className='label'>
+        Last Name
+         <input 
+          className='input'
+         type="text" 
+         name='last_name'
+         value={values.last_name}
+         onChange={(e) =>handleChange(e.target)}
+         />
+        </label>
+      </div>
+      <div className='mb-4'>
+        <label className='label'>
+        Email
+         <input 
+          className='input'
+          type="email"
+          name='email'
+          value={values.email}
+          onChange={(e) => handleChange(e.target)}
+          />
+        </label>
+      </div>
+      <div className='mb-4'>
+        <label className='label'>
+        Password
+         <input
+          className='input'
+          type="password" 
+          name='password'
+          value={values.password}
+          onChange={(e) => handleChange(e.target)}
+         />
+        </label>
+      </div>
+      <div className='mb-4'>
+        <label className='label'>
+        Birthday
+         <input
+          className='input' 
+         type="date"
+         name='birthday'
+         value={values.birthday}
+         onChange={(e) => handleChange(e.target)}
+          />
+        </label>
+      </div>
+      <div className='mb-4'>
+        <label className='label'>
+        Image URL
+         <input 
+         className='input'
+          type="text" 
+          name='image_url'
+          value={values.image_url}
+          onChange={(e) => handleChange(e.target)}
+          />
+        </label>
+      </div>
+   
+     <button type='submit'  className='btn'>Create</button>
+    </form>
+     
+     {pending ? <p>Loading...</p> : 
+     <ul>
+      {users &&  users.map(user =>
+         <div  className='mt-5 px-4'>
+          <ul key={user.id}>
+
+            <li >First Name: {user.first_name}</li>
+
+          <li >{user.last_name}</li>
+          <li >{user.email}</li>
+          <li>{user.password}</li>
+          <li >{user.birthday}</li>
+          <li >{user.image_url}</li>
+          <br />
+          </ul>
+          
+          <div className='mb-2'>
+          <button onClick={ () => handleEdit(user)} className='btn bg-amber-300  ml-1 mr-1'>Edit</button>
+          <button onClick={() => remove(user.id) } className='btn bg-red-500 text-white'>Delete</button>
+          </div>
+
+         </div>
+        
+    
+       )}
+     </ul>
+  
+
+
+
+   
+     }
+    
+    </div>
+  )
+}
+
+export default App
---
+++ src/hooks/useCrudApi.jsx
@@ -0,0 +1,57 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+
+function useCrudApi() {
+   const [data, setData]= useState([])
+   const [pending, setPending]= useState(false)
+   const [error, setError]= useState(null)
+   
+   const request = async ({ url, method = 'GET', body = null, id = null }) => {
+    setPending(true);
+    setError(null);
+  
+    // Normalize method to uppercase
+    const normalizedMethod = method.toUpperCase();
+    
+  
+    try {
+      const res = await axios({
+        url,
+        method: normalizedMethod,
+        data: normalizedMethod !== 'GET' ? body : null
+      }); 
+      
+      switch (normalizedMethod) {
+         
+        case 'POST':
+         setData((prev) =>[...prev, res.data])
+          
+          break
+  
+        case 'PUT':
+        case 'PATCH':
+          setData((prev) => prev.map(i =>i.id === res.data.id ? res.data : i));
+          break;
+  
+        case 'DELETE':
+          setData((prev) => prev.filter(i => i.id !== id));
+          break;
+  
+        default:
+          setData(res.data.results);
+      }
+    } catch (error) {
+      console.log(error)
+      setError(error.message);
+    } finally {
+      setPending(false);
+    }
+  };
+  
+  
+
+  return {data,request, pending,error} 
+  
+}
+
+export default useCrudApi
